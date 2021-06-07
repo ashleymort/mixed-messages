ec5b1f67d5c808104c96203d54bcf783595cf9dc
Ashley M <ashleymort@hotmail.co.uk>
2021-06-07 09:35:09 +0100

    gave id's to html paragraphs, wrote random number generator in script

---
+++ script.js
@@ -0,0 +1,15 @@
+let planet = ['Moon', 'Venus', 'Mars', 'Mercury', 'Saturn', 'Jupiter', 'Neptune'];
+
+let luck = ['You will be extra lucky today!!', 'You will be out of luck today!!',
+    'You will be super unlucky today!!', 'Luck may not be on your side!!',
+    'Good luck you are going to need it!!', 'Your luck might just be in!!',
+    'Be lucky', 'Best of Luck you are going to need it!!'];
+
+let tips = ['Watch out for black cats', 'Avoid the Sea', 'Drink plenty of water',
+    'Five a day keeps you fit and healthy', 'An apple a day keeps the doctor away',
+    'If you see a solo magpie salute it', 'Go for that run', 'Keep things simple',
+    'A change is as good as a rest', 'Treat your loved ones'];
+
+let generateRandomNum = Math.floor(Math.random()) * 10;
+
+document.write(generateRandomNum);
